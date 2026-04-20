e3f226df6dc44e978318a7679bbb817f64b06d57
Anirudh <anirudhpatwal19@gmail.com>
2026-04-20 13:07:03 +0530

    cse 12

---
+++ twone/src/App.jsx
@@ -0,0 +1,20 @@
+import React, {useRef} from 'react';
+function App() {
+  const inputRef = useRef();
+  const hideInput = () => {
+    inputRef.current.style.display = 'none';
+  };
+  const showInput = () => {
+    inputRef.current.style.display = 'block';
+  };
+
+  return (
+    <div>
+      <input ref={inputRef} type="text" placeholder="Hello" />
+      <button onClick={hideInput}>Hide</button>
+      <button onClick={showInput}>Show</button>
+    </div>
+  )
+}
+
+export default App;
